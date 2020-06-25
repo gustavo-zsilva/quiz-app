997688ec258b3fddb0d1b866538a45e9b79c7791
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-06-25 19:34:08 -0300

    Commit scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,24 @@
 const question = document.querySelector('.question h1')
+const next = document.querySelector('.content .next')
 const ul = document.querySelector('.content .options ul')
-const options = {}
-const numOptions = 2
+const buttons = []
+const numOptions = 4
 
-if (numOptions === 2) {
-    for (var i = 0; i <= 1; i++) {
-        var button = document.createElement('button')
-        ul.appendChild(button)
+
+var fillOptions = numOptions === 2 ? fill(2) : fill(4);
+
+function fill(n) {
+    for (var i = 1; i <= n; i++) {
+        var li = document.createElement('li')
+        buttons[i] = document.createElement('button')
+        li.appendChild(buttons[i])
+        ul.appendChild(li)
     }
-
 }
+
+buttons.forEach(() => {
+    addEventListener('click', () => {
+        
+        next.classList.remove('hide')
+    })
+})
